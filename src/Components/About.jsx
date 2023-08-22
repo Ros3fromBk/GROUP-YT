be94407e199d2b06083d0da108c4a8dc9d1f295e
Jeremy Montes <jeremymontes@pursuit.org>
2023-08-22 11:15:05 -0400

    added background img, edited css for ShowPage, edited display for rendered videos

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,24 +2,19 @@ import "./About.css"
 import github from "../assets/github-mark.svg"
 function About({ engineers }) {
 
-
     return (
-                
 
         <div className="AboutMe">
             <div className="space">
-            <h3>{engineers.name}</h3>
-
-            <img src={engineers.pic} alt={engineers.name} />
-            <h5>{engineers.hometown}</h5>
-
-            <h5>{engineers.fact}</h5>
-            <a href={engineers.github}>
-            <img className="github" src={github}/>
-            </a>
+                <h3>{engineers.name}</h3>
+                <img src={engineers.pic} alt={engineers.name} />
+                <h5>{engineers.hometown}</h5>
+                <h5>{engineers.fact}</h5>
+                <a href={engineers.github}>
+                    <img className="github" src={github} />
+                </a>
             </div>
         </div>
-   
 
     )
 }
